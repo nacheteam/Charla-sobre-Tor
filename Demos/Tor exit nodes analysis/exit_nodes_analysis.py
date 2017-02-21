8f9a13c540fad193cb546c2d27dbc6275d71c682
Ignacio Aguilera Martos <nacheteam@nacheteam-Equipo>
2017-02-21 16:01:27 +0100

    Añadido el subprocess con nmap

--- Demos/Tor exit nodes analysis/exit_nodes_analysis.py
+++ Demos/Tor exit nodes analysis/exit_nodes_analysis.py
@@ -2,11 +2,15 @@ import os
 import csv
 import subprocess
 
+ips_nodos_salida = []
+
 os.system("wget https://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv")
-with open("Tor_ip_list_EXIT.csv", "rb") as exit_list:
+with open("Tor_ip_list_EXIT.csv", 'r') as exit_list:
     exit_list_reader = csv.reader(exit_list, delimiter=' ', quotechar='|')
     for linea in exit_list_reader:
-        print(linea)
+        ips_nodos_salida.append(linea)
 
 os.system("rm Tor_ip_list_EXIT.csv")
-
+for ip in ips_nodos_salida:
+    comando = "nmap -vv -A --reason " + ip
+    nmap_nodo = subprocess.Popen(['/bin/sh', '-c', comando], stdout=subprocess.PIPE)
